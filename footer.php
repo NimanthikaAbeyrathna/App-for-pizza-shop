8e11d514ce3ff0fb8f447fcbb5b594bfb887e728
Nimanthika <nimanthikaabeyrathna@gmail.com>
2025-05-23 23:15:38 -0700

    Created requred pages

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+</head>
+<body>
+    <div class="footer">
+        <div class="p-8 bg-yellow-600 decoration-gray-50 text-sm/6 flex justify-center">
+            <h6>© 2025 Nimanthika Abeyrathna. All rights reserved.</h6>
+            
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar</title>
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <div class="navbar">
+        <div class="p-8 bg-yellow-600 decoration-gray-50 text-sm/6 flex justify-between">
+
+            <div class="flex items-center space-x-4">
+                <img src="./images/pizzaHutLogo.jpg" alt="Pizza Hut Logo" class="w-12 h-12 rounded-full" />
+                <h1 class="text-xl font-bold">Pizza Hut</h1>
+            </div>
+            <a href="./addpizza.php" class="bg-black hover:bg-blue-500 text-center text-white py-1 
+            px-3 rounded-md flex items-center justify-center">Add Pizza</a>
+
+            
+        </div>
+    </div>
+
+    
+</body>
+</html>
